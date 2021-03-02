c71505becfc62abc63010fc7b3f86ad30c567bbe
Ho Wai Kin Johnny <johnnyhohohohohoho@gmail.com>
2021-03-02 11:32:32 +0800

    add spinner

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,33 @@
 import React, {useEffect} from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, ActivityIndicator, View, StyleSheet } from 'react-native';
 import ProductItem from '../components/product-item';
 import { connect } from 'react-redux';
 import { fetchAllProducts } from '../redux/productActions';
-import { setAllProducts } from '../redux/productActions';
 
 const Home = ({navigation, fetchAllProducts, data, loading, error}) => {
-    useEffect(() => {
-        fetchAllProducts();
-    }, [])
+    useEffect(() => fetchAllProducts(), [])
 
     return (
-        <ScrollView>
-            {loading?
-                <Text>Loading...</Text> :
-                error.message ?
-                    <Text>Error in fetching products</Text>
-                :
-                    data.map((p, i) => <ProductItem key={i} id={i} title={p.title} navigation={navigation}/>)
-            }
-        </ScrollView>
+        loading?
+            <View style={styles.container}>
+                <ActivityIndicator size='large'/>
+            </View>
+        :
+            error.message ?
+                <Text>Error in fetching products</Text>
+            :
+                <ScrollView>
+                    {data.map((p, i) => <ProductItem key={i} id={i} title={p.title} navigation={navigation}/>)}
+                </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    }
+})
 
 const mapStateToProps = state => state.products;
 
