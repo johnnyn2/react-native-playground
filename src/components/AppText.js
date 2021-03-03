b77ede14ddad26c8984e8c9c090695424102f88a
Ho Wai Kin Johnny <johnnyhohohohohoho@gmail.com>
2021-03-03 14:54:03 +0800

    use ReadMore component but seems not working

--- src/components/AppText.js
+++ src/components/AppText.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+import ReadMore from 'react-native-read-more-text';
 
 const AppText = (props) => {
-    const {defaultStyles, style, text} = props;
-    return <Text style={[defaultStyles, style]}>{text}</Text>
+    const { defaultStyles, style, text, readMore } = props;
+    const textEle = (
+        <Text style={[defaultStyles, style]}>
+            {text}
+        </Text>
+    );
+
+    const _renderTruncatedFooter = (handlePress) => {
+        return (
+            <Text style={{ color: Colors.tintColor, marginTop: 5 }} onPress={handlePress}>
+                Read more
+            </Text>
+        );
+    }
+
+    const _renderRevealedFooter = (handlePress) => {
+        return (
+            <Text style={{ color: Colors.tintColor, marginTop: 5 }} onPress={handlePress}>
+                Show less
+            </Text>
+        );
+    }
+
+    return (
+        readMore ?
+            <View style={{flex: 1}}><ReadMore
+                numberOfLines={3}
+                renderTruncatedFooter={fn => _renderTruncatedFooter(fn)}
+                renderRevealedFooter={fn => _renderRevealedFooter(fn)}
+            >
+                {textEle}
+            </ReadMore></View> :
+            textEle
+    )
 }
 
 AppText.defaultProps = {
     defaultStyles: {
-        
+
     }
 }
 
